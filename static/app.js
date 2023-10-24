e9f1bc75a0bdb168aaaee46ffcaafd3170f85a8e
Leor Seal <leor.seal@gmail.com>
2023-10-24 10:12:39 -0400

    Dashboard Finalizing

--- static/app.js
+++ static/app.js
@@ -1,117 +1,105 @@
-let diabetesData;
-
 document.addEventListener("DOMContentLoaded", function() {
-    const idDropdown = d3.select('#selDataset');
-    const columnDropdown = d3.select('#anotherDropdown');
+  const idDropdown = d3.select('#selDataset');
+  const columnDropdown = d3.select('#anotherDropdown');
 
-    // Fetch the initial data
-    fetchData();
-
-    // Assuming the data is fetched synchronously (this might not be the case, and adjustments would be needed)
-    let initialData = diabetesData.find(data => data.Id == idDropdown.property('value'));
-    displayDemographicInfo(initialData);
-    updateBarChart(idDropdown.property('value'), columnDropdown.property('value'));
+  let initialData = diabetesData.find(data => data.Id == idDropdown.property('value'));
+  displayDemographicInfo(initialData);
+  updateBarChart(idDropdown.property('value'), columnDropdown.property('value'));
 });
 
 function optionChanged(id) {
-    const column = d3.select('#anotherDropdown').property('value');
-    updateBarChart(id, column);
+  const column = d3.select('#anotherDropdown').property('value');
+  updateBarChart(id, column);
 
-    // Get the selected data based on the chosen ID
-    let selectedData = diabetesData.find(data => data.Id == id);
-    displayDemographicInfo(selectedData);
+  // Get the selected data based on the chosen ID
+  let selectedData = diabetesData.find(data => data.Id == id);
+  displayDemographicInfo(selectedData);
 }
 
 function anotherOptionChanged(column) {
-    const id = d3.select('#selDataset').property('value');
-    updateBarChart(id, column);
-}
-
-function fetchData() {
-    // Fetching data from the "/" endpoint using GET
-    fetch('/')
-        .then(response => response.json())
-        .then(data => {
-            diabetesData = data.records;
-            init();
-        })
-        .catch(error => console.error('Error fetching data:', error));
-}
-
-function init() {
-    const dropdown = d3.select("#selDataset");
-    diabetesData.forEach(record => {
-        dropdown.append("option").text(record.Id).property("value", record.Id);
-    });
+  const id = d3.select('#selDataset').property('value');
+  updateBarChart(id, column);
 }
 
 function updateBarChart(id, column) {
-    const formData = new FormData();
-    formData.append('id', id);
-    formData.append('column', column);
+  const formData = new FormData();
+  formData.append('id', id);
+  formData.append('column', column);
 
-    // Posting data to the same "/" endpoint for processing and getting back bar chart data
-    fetch('/', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        const trace = {
-            x: ['Id', 'Mean Diabetes Positive', 'Mean Diabetes Negative'],
-            y: [data.individual, data.mean_positive, data.mean_negative],
-            type: 'bar',
-            marker: {
-                color: ['SteelBlue', 'DarkSalmon', 'MediumSeaGreen']
-            },
-            width: [0.4, 0.4, 0.4]
-        };
-        const layout = {
-            title: {
-                text:`Comparison of ID ${id} vs Mean of Sample Group`,
-                font: {
-                    size: 19,
-                    color: 'black'
-                }
-            },
-            xaxis: {
-                title: {
-                    text: 'Category',
-                    font: {
-                        size: 16,
-                        color: 'black'
-                    }
-                },
-                tickfont: {
-                    size: 16,
-                    color: 'black'
-                }
-            },
-            yaxis: {
-                title: {
-                    text: column,
-                    font: {
-                        size: 16,
-                        color: 'black'
-                    }
-                },
-                tickfont: {
-                    size: 16,
-                    color: 'black'
-                }
-            },
-            barmode: 'group',
-            bargap: 0.15,
-            bargroupgap: 0.1
-        };
-        Plotly.newPlot('bar', [trace], layout);
-    });
+  // Posting data to the same "/" endpoint for processing and getting back bar chart data
+  fetch('/', {
+      method: 'POST',
+      body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+      const trace = {
+          x: ['Id', 'Mean Diabetes Positive', 'Mean Diabetes Negative'],
+          y: [data.individual, data.mean_positive, data.mean_negative],
+          type: 'bar',
+          marker: {
+              color: ['SteelBlue', 'DarkSalmon', 'MediumSeaGreen']
+          },
+          width: [0.4, 0.4, 0.4]
+      };
+
+      const layout = {
+          title: {
+              text:`Comparison of ID ${id} vs Mean of Sample Group`,
+              font: {
+                  size: 19,
+                  color: 'black'
+              }
+          },
+          xaxis: {
+              title: {
+                  text: 'Category',
+                  font: {
+                      size: 16,
+                      color: 'black'
+                  }
+              },
+              tickfont: {
+                  size: 16,
+                  color: 'black'
+              },
+              tickangle: -10
+          },
+          yaxis: {
+              title: {
+                  text: column,
+                  font: {
+                      size: 16,
+                      color: 'black'
+                  }
+              },
+              tickfont: {
+                  size: 16,
+                  color: 'black'
+              }
+          },
+          barmode: 'group',
+          bargap: 0.15,
+          bargroupgap: 0.1,
+          width: 785,  // Set the width of the graph
+          height: 600  // Set the height of the graph
+      };
+
+      Plotly.newPlot('bar', [trace], layout);
+  });
 }
 
-function displayDemographicInfo(metadata) {
-    const sampleMetadata = d3.select("#sample-metadata");
-    sampleMetadata.html('');
-    Object.entries(metadata).forEach(([key, value]) => {
-        sampleMetadata.append('p').text(`${key}: ${value}`);
-    });
+function displayDemographicInfo(data) {
+  let demographicInfoBox = d3.select("#sample-metadata");
+
+  // First, clear out any existing data
+  demographicInfoBox.html("");
+
+  // Now append key-value pairs to the panel
+  Object.entries(data).forEach(([key, value]) => {
+      demographicInfoBox.append("h6").text(`${key}: ${value}`);
+  });
 }
+
+
+
